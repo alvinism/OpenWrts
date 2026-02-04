815756971d9acbba018b710acafc30812b8b61d0
alvin <ychx.alvin@gmail.com>
2026-02-04 22:52:31 +0900

    Remove iStore, add Tailscale CLI
    - Remove iStore feed (closed-source security concern)
    - Remove luci-app-store from Standard and Lite configs
    - Add tailscale package for remote VPN access

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 # Modify default system settings
 
 # 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate 
 
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -10,6 +10,3 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 # passwall
 echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> feeds.conf.default
-
-# iStore
-echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
